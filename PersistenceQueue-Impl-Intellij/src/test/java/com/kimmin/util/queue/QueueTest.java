8a13f32b7db812be1f7b92d7bdf2d1d106e6657e
yue9944882 <291271447@qq.com>
2016-02-26 08:49:55 -0500

    Queue Travis

---
+++ PersistenceQueue-Impl-Intellij/src/test/java/com/kimmin/util/queue/QueueTest.java
@@ -0,0 +1,16 @@
+package com.kimmin.util.queue;
+
+import org.junit.Test;
+
+/**
+ * Created by kimmin on 2/26/16.
+ */
+public class QueueTest {
+
+    @Test
+    public void doTest(){
+        /** Fake Test **/
+        System.err.println("Test");
+    }
+
+}
